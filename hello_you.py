2152546f0a7398f937cd3d097bc5d96c7f5de4f0
Shivayan Bora <bora.shivayan@gmail.com>
2018-04-14 01:49:51 +0530

    first commit

---
+++ hello_you.py
@@ -0,0 +1,20 @@
+# The input function is a Python inbuilt function 
+# and it always returns a string
+# Ask user for name
+name = input("What is your name?: ")
+
+# Ask user for their age
+age = input("How old are you?: ")
+
+# Ask user for city
+city = input("What city do you live in?: ")
+
+# Ask user what they enjoy
+love = input("What do you love doing?: ")
+
+# Create output text
+string = "Your name is {} and you are {} years old. You live in {} and you love {}"
+output = string.format(name, age, city, love)
+
+# Print output to screen
+print(output)
